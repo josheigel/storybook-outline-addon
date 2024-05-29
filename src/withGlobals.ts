093cafa0930715fec5e40ff3d710086ac440089b
Josiah Heigel <josheigel@gmail.com>
2024-05-29 13:12:02 -0400

    Added outline functionality

--- src/withGlobals.ts
+++ src/withGlobals.ts
@@ -3,8 +3,12 @@ import type {
   PartialStoryFn as StoryFunction,
   StoryContext,
 } from "@storybook/types";
-import { useEffect, useGlobals } from "@storybook/preview-api";
+import { useEffect, useMemo, useGlobals } from "@storybook/preview-api";
 import { PARAM_KEY } from "./constants";
+
+import { clearStyles, addOutlineStyles } from './helpers';
+
+import outlineCSS from './OutlineCSS';
 
 export const withGlobals = (
   StoryFn: StoryFunction<Renderer>,
@@ -12,50 +16,29 @@ export const withGlobals = (
 ) => {
   const [globals] = useGlobals();
   const myAddon = globals[PARAM_KEY];
-  // Is the addon being used in the docs panel
   const isInDocs = context.viewMode === "docs";
-  const { theme } = context.globals;
+  const isActive = [true, 'true'].includes(myAddon);
+
+  const outlineStyles = useMemo(() => {
+    const selector = isInDocs ? `#anchor--${context.id} .sb-story` : '#storybook-root';
+
+    return outlineCSS(selector);
+  }, [context.id]);
 
   useEffect(() => {
-    // Execute your side effect here
-    // For example, to manipulate the contents of the preview
-    const selector = isInDocs
-      ? `#anchor--${context.id} .sb-story`
-      : "#storybook-root";
+    const selectorId = isInDocs ? `my-addon-docs-${context.id}` : `my-addon`;
 
-    displayToolState(selector, {
-      myAddon,
-      isInDocs,
-      theme,
-    });
-  }, [myAddon, theme]);
+    if (!isActive) {
+      clearStyles(selectorId);
+      return;
+    }
+
+    addOutlineStyles(selectorId, outlineStyles);
+
+    return () => {
+      clearStyles(selectorId);
+    };
+  }, [isActive, outlineStyles, context.id]);
 
   return StoryFn();
 };
-
-function displayToolState(selector: string, state: any) {
-  const rootElements = document.querySelectorAll(selector);
-
-  rootElements.forEach((rootElement) => {
-    let preElement = rootElement.querySelector<HTMLPreElement>(
-      `${selector} pre`
-    );
-
-    if (!preElement) {
-      preElement = document.createElement("pre");
-      preElement.style.setProperty("margin-top", "2rem");
-      preElement.style.setProperty("padding", "1rem");
-      preElement.style.setProperty("background-color", "#eee");
-      preElement.style.setProperty("border-radius", "3px");
-      preElement.style.setProperty("max-width", "600px");
-      preElement.style.setProperty("overflow", "scroll");
-      rootElement.appendChild(preElement);
-    }
-
-    preElement.innerText = `This snippet is injected by the withGlobals decorator.
-It updates as the user interacts with the ⚡ or Theme tools in the toolbar above.
-
-${JSON.stringify(state, null, 2)}
-`;
-  });
-}
